d39f42374db0714f35c6b2cb0795a50016472f74
Tuskemx <ma.edwards@live.com>
2019-05-18 14:01:57 +0100

    heroku

--- db/data/index.js
+++ db/data/index.js
@@ -3,9 +3,11 @@ const ENV = process.env.NODE_ENV || 'development';
 const development = require('./dev-data');
 const test = require('./test-data');
 
+
 const data = {
   development,
   test,
+  production: development
 };
 
 module.exports = data[ENV];
