c08e1aada6f1884043ed5f17ab846ba38b3b0ea3
Tuskemx <ma.edwards@live.com>
2019-05-18 11:26:19 +0100

    tests now uniform in response.body

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -21,9 +21,9 @@ describe('/api', () => {
             return request(app)
                 .get('/api/topics')
                 .expect(200)
-                .then((result) => {
-                    expect(result.body.topics).to.be.an('array');
-                    expect(result.body.topics[0]).to.have.keys('description', 'slug');
+                .then((response) => {
+                    expect(response.body.topics).to.be.an('array');
+                    expect(response.body.topics[0]).to.have.keys('description', 'slug');
                 })
         })
     })
@@ -32,10 +32,10 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles')
                 .expect(200)
-                .then((result) => {
-                    expect(result.body.articles).to.be.an('array');
-                    expect(result.body.articles).to.have.lengthOf(12);
-                    expect(result.body.articles[0]).to.have.keys(
+                .then((response) => {
+                    expect(response.body.articles).to.be.an('array');
+                    expect(response.body.articles).to.have.lengthOf(12);
+                    expect(response.body.articles[0]).to.have.keys(
                         'author',
                         'title',
                         'article_id',
@@ -52,9 +52,9 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles?author=butter_bridge')
                 .expect(200)
-                .then((result) => {
-                    const len = result.body.articles.length - 1;
-                    expect(result.body.articles[len].author).to.equal('butter_bridge');
+                .then((response) => {
+                    const len = response.body.articles.length - 1;
+                    expect(response.body.articles[len].author).to.equal('butter_bridge');
                 });
         });
     });
@@ -63,10 +63,10 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles')
                 .expect(200)
-                .then((result) => {
-                    
-                    expect(dateRemoveLetters(result.body.articles[0].created_at)).to.greaterThan(dateRemoveLetters(result.body.articles[5].created_at))
-                    
+                .then((response) => {
+
+                    expect(dateRemoveLetters(response.body.articles[0].created_at)).to.greaterThan(dateRemoveLetters(response.body.articles[5].created_at))
+
                 });
         });
     });
@@ -75,9 +75,9 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles?sort_by=comment_count')
                 .expect(200)
-                .then((result) => {
-                    const len = result.body.articles.length - 1;
-                    expect(Number(result.body.articles[0].comment_count)).to.greaterThan(Number(result.body.articles[len].comment_count));
+                .then((response) => {
+                    const len = response.body.articles.length - 1;
+                    expect(Number(response.body.articles[0].comment_count)).to.greaterThan(Number(response.body.articles[len].comment_count));
                 });
         });
     });
@@ -86,8 +86,8 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles?topic=mitch')
                 .expect(200)
-                .then((result) => {
-                    expect(result.body.articles[0].topic).to.eql(result.body.articles[1].topic);
+                .then((response) => {
+                    expect(response.body.articles[0].topic).to.eql(response.body.articles[1].topic);
                 });
         });
     });
@@ -96,8 +96,8 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles?author=not-an-author')
                 .expect(404)
-                .then(({ body }) => {
-                    expect(body.msg).to.eql('Articles Not Found');
+                .then((response) => {
+                    expect(response.body.msg).to.eql('Articles Not Found');
                 });
         });
     });
@@ -129,9 +129,9 @@ describe('/api', () => {
                 .patch('/api/articles/1')
                 .send({ inc_votes: 5 })
                 .expect(200)
-                .then((result) => {
-                    expect(result.body.article.article_id).to.eql(1);
-                    expect(result.body.article.votes).to.eql(105);
+                .then((response) => {
+                    expect(response.body.article.article_id).to.eql(1);
+                    expect(response.body.article.votes).to.eql(105);
                 });
         });
 
@@ -141,11 +141,11 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles/5/comments')
                 .expect(200)
-                .then((res) => {
+                .then((response) => {
 
-                    expect(res.body.length).to.equal(2);
-                    expect(res.body[0]).to.contain.keys('comment_id', 'author', 'votes', 'created_at', 'body', 'article_id');
-                    expect(res.body[0].body).to.eql('What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.')
+                    expect(response.body.length).to.equal(2);
+                    expect(response.body[0]).to.contain.keys('comment_id', 'author', 'votes', 'created_at', 'body', 'article_id');
+                    expect(response.body[0].body).to.eql('What do you see? I have no idea where this will lead us. This place I speak of, is known as the Black Lodge.')
                 })
         })
     });
@@ -154,13 +154,13 @@ describe('/api', () => {
             return request(app)
                 .get('/api/articles/1/comments?sort_by=votes&order=asc')
                 .expect(200)
-                .then((res) => {
-                    expect(res.body.length).to.equal(13);
-                    const len = res.body.length;
-                    expect(res.body[0].votes).to.be.lessThan(res.body[len - 1].votes)
-                    expect(res.body[1].votes).to.be.lessThan(res.body[len - 1].votes)
-                    expect(res.body[5].votes).to.be.lessThan(res.body[len - 1].votes)
-                    expect(res.body[6].votes).to.be.lessThan(res.body[len - 3].votes)
+                .then((response) => {
+                    expect(response.body.length).to.equal(13);
+                    const len = response.body.length;
+                    expect(response.body[0].votes).to.be.lessThan(response.body[len - 1].votes)
+                    expect(response.body[1].votes).to.be.lessThan(response.body[len - 1].votes)
+                    expect(response.body[5].votes).to.be.lessThan(response.body[len - 1].votes)
+                    expect(response.body[6].votes).to.be.lessThan(response.body[len - 3].votes)
                 })
         })
     });
@@ -173,10 +173,10 @@ describe('/api', () => {
                     body: 'TESSSSSSSSSSSSSSTTTTTTTTT'
                 })
                 .expect(200)
-                .then((result) => {
+                .then((response) => {
 
-                    expect(result.body).to.have.keys('comm');
-                    expect(result.body.comm[0].article_id).to.eql(2);
+                    expect(response.body).to.have.keys('comm');
+                    expect(response.body.comm[0].article_id).to.eql(2);
                     // no article id 
 
                 });
@@ -191,10 +191,10 @@ describe('/api', () => {
                     inc_votes: 500
                 })
                 .expect(200)
-                .then((result) => {
+                .then((response) => {
 
-                    console.log(result.body.comment.votes);
-                    expect(result.body.comment.votes).to.eql(600);
+                    console.log(response.body.comment.votes);
+                    expect(response.body.comment.votes).to.eql(600);
                     // no article id 
 
                 });
@@ -205,10 +205,10 @@ describe('/api', () => {
             return request(app)
                 .delete('/api/comments/3')
                 .expect(204)
-                .then((result) => {
+                .then((response) => {
 
-                    console.log(result.body);
-                    expect(result.body).to.eql({});
+                    console.log(response.body);
+                    expect(response.body).to.eql({});
                     // no article id 
 
                 });
@@ -219,11 +219,10 @@ describe('/api', () => {
             return request(app)
                 .get('/api/users')
                 .expect(200)
-                .then(({ body }) => {
-                    console.log(body);
-                    expect(body.users).to.be.an('array');
-                    expect(body.users[0].username).to.eql('butter_bridge');
-                    expect(body.users).to.have.length(4);
+                .then((response) => {
+                    expect(response.body.users).to.be.an('array');
+                    expect(response.body.users[0].username).to.eql('butter_bridge');
+                    expect(response.body.users).to.have.length(4);
                 });
         });
     });
@@ -233,10 +232,10 @@ describe('/api', () => {
             return request(app)
                 .get('/api/users/butter_bridge')
                 .expect(200)
-                .then(({ body }) => {
-                    expect(body.user).to.be.an('object');
-                    expect(body.user.username).to.eql('butter_bridge');
-                    expect(body.user.name).to.eql('jonny');
+                .then((response) => {
+                    expect(response.body.user).to.be.an('object');
+                    expect(response.body.user.username).to.eql('butter_bridge');
+                    expect(response.body.user.name).to.eql('jonny');
 
                 });
         });
