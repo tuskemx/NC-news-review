b6ccb56e133c6188beb1c3b7d872d2b1f3e29cc1
Tuskemx <ma.edwards@live.com>
2019-05-18 13:32:50 +0100

    error testing

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,8 @@
+exports.handle400s = (err, req, res, next) => {
+  if (err.code === '23503') res.status(400).send({ msg: 'Bad Request' });
+  else next(err);
+};
+
 exports.routeNotFound = (req, res) => {
   res.status(404).send({ msg: 'Route Not Found' });
 };
