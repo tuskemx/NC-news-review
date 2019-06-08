f76e2efb0b87dfb2cbf45d488d8d31f1fe5ba6a9
tuskemx <clubpelicone@gmail.com>
2019-06-08 11:55:54 +0100

    err

--- errors/index.js
+++ errors/index.js
@@ -1,8 +1,10 @@
 exports.handle400s = (err, req, res, next) => {
   if (err.code === '23503') res.status(400).send({ msg: 'Bad Request' });
-  if (err.code === '42703') res.status(400).send({ msg: 'invalid Sort_By Query' });
-  if (err.code === '22P02') res.status(400).send({ msg: 'bad request check input query' });
-  if (err.code === '22003') res.status(404).send({ msg: 'article Not Found' });
+  if (err.code === '22P02') res.status(400).send({ msg: 'Invalid Article ID' });
+  if (err.code === '22003') res.status(404).send({ msg: 'Article Not Found' });
+  if (err.code === '23505') res.status(422).send({ msg: 'Topic Already Exists' });
+  if (err.code === '23502') res.status(422).send({ msg: 'Topic Description Required' });
+  if (err.code === '42703') res.status(400).send({ msg: 'Invalid Sort_By Query' });
   else next(err);
 };
 
